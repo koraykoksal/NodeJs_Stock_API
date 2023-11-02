b6d3a2acd3603e5ea1476596edff07fe062f3692
koraykoksal <kksl.koray@gmail.com>
2023-11-02 22:40:23 +0300

    sales update

--- src/controllers/sale.js
+++ src/controllers/sale.js
@@ -78,6 +78,7 @@ module.exports = {
             #swagger.summary = "Get Single Sale"
         */
 
+        // Read:
         const data = await Sale.findOne({ _id: req.params.id }).populate(['brand_id', 'product_id'])
 
         res.status(200).send({
@@ -97,6 +98,24 @@ module.exports = {
             }
         */
 
+        if (req.body?.quantity) {
+            // get current stock quantity from the Sale:
+            const currentSale = await Sale.findOne({ _id: req.params.id })
+            // different:
+            const quantity = req.body.quantity - currentSale.quantity
+            // console.log(quantity)
+            // set stock (quantity) when Sale process:
+            const updateProduct = await Product.updateOne({ _id: currentSale.product_id, stock: { $gte: quantity } }, { $inc: { stock: -quantity } })
+            // console.log(updateProduct)
+            
+            // if stock limit not enough:
+            if (updateProduct.modifiedCount == 0) { // Check Limit
+                res.errorStatusCode = 422
+                throw new Error('There is not enough stock for this sale.')
+            }
+        }
+
+        // Update:
         const data = await Sale.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
 
         res.status(202).send({
